7993b10a7c5d14cc18084edbe9bb7d4dc5428474
Chetan Sarva <chetan@pixelcop.net>
2012-01-09 17:30:05 -0500

    re-indent 2sp

--- src/main/java/net/pixelcop/sewer/ByteArrayEvent.java
+++ src/main/java/net/pixelcop/sewer/ByteArrayEvent.java
@@ -12,39 +12,39 @@ import java.io.IOException;
  */
 public class ByteArrayEvent implements Event {
 
-    private static final String NULL = "null";
+  private static final String NULL = "null";
 
-    private byte[] body; // TODO use re-usable buffer for reading sequence files
+  private byte[] body; // TODO use re-usable buffer for reading sequence files
 
-    public ByteArrayEvent() {
+  public ByteArrayEvent() {
+  }
+
+  public ByteArrayEvent(byte[] body) {
+    this.body = body;
+  }
+
+  public ByteArrayEvent(DataInput in) throws IOException {
+    readFields(in);
+  }
+
+  @Override
+  public void write(DataOutput out) throws IOException {
+    out.writeInt(body.length);
+    out.write(body);
+  }
+
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    this.body = new byte[in.readInt()];
+    in.readFully(body);
+  }
+
+  @Override
+  public String toString() {
+    if (body == null) {
+      return NULL;
     }
-
-    public ByteArrayEvent(byte[] body) {
-        this.body = body;
-    }
-
-    public ByteArrayEvent(DataInput in) throws IOException {
-        readFields(in);
-    }
-
-    @Override
-    public void write(DataOutput out) throws IOException {
-        out.writeInt(body.length);
-        out.write(body);
-    }
-
-    @Override
-    public void readFields(DataInput in) throws IOException {
-        this.body = new byte[in.readInt()];
-        in.readFully(body);
-    }
-
-    @Override
-    public String toString() {
-      if (body == null) {
-        return NULL;
-      }
-      return new String(body);
-    }
+    return new String(body);
+  }
 
 }

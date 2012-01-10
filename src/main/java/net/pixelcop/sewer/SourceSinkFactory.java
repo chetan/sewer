22997da132c19d532759119a3f4a66d8065823aa
Chetan Sarva <chetan@pixelcop.net>
2012-01-09 23:33:11 -0500

    fixed a NPE

--- src/main/java/net/pixelcop/sewer/SourceSinkFactory.java
+++ src/main/java/net/pixelcop/sewer/SourceSinkFactory.java
@@ -30,11 +30,13 @@ public class SourceSinkFactory<T> {
       StringBuilder sb = new StringBuilder();
       sb.append(clazz.getSimpleName());
       sb.append("(");
-      for (int i = 0; i < args.length; i++) {
-        if (i > 0) {
-          sb.append(", ");
+      if (args != null) {
+        for (int i = 0; i < args.length; i++) {
+          if (i > 0) {
+            sb.append(", ");
+          }
+          sb.append(args[i]);
         }
-        sb.append(args[i]);
       }
       sb.append(")");
       return sb.toString();

0d2a78ce34a347fb24496bcbe825ee9a5486b1e0
Chetan Sarva <chetan@pixelcop.net>
2012-01-10 00:26:59 -0500

    added source suite

--- src/test/java/net/pixelcop/sewer/SewerTestSuite.java
+++ src/test/java/net/pixelcop/sewer/SewerTestSuite.java
@@ -1,13 +1,14 @@
 package net.pixelcop.sewer;
 
 import net.pixelcop.sewer.node.TestNodeWiring;
+import net.pixelcop.sewer.source.SourceTestSuite;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestSourceSinkFactory.class, TestNodeWiring.class })
+@SuiteClasses({ TestSourceSinkFactory.class, TestNodeWiring.class, SourceTestSuite.class })
 public class SewerTestSuite {
 
 }
---
+++ src/test/java/net/pixelcop/sewer/source/SourceTestSuite.java
@@ -0,0 +1,11 @@
+package net.pixelcop.sewer.source;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestHttpPixelSource.class })
+public class SourceTestSuite {
+
+}

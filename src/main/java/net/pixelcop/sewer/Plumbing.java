87d97229500075971b25342bfc0a6bb1df5768b1
Chetan Sarva <chetan@pixelcop.net>
2012-01-10 22:26:47 -0500

    added helper getStatusString()

--- src/main/java/net/pixelcop/sewer/Plumbing.java
+++ src/main/java/net/pixelcop/sewer/Plumbing.java
@@ -2,9 +2,20 @@ package net.pixelcop.sewer;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class Plumbing implements Closeable, StatusProvider {
+
+  public static final Map<Integer, String> STATUS_MAP = new HashMap<Integer, String>();
+  static {
+    STATUS_MAP.put(CLOSED,  "CLOSED");
+    STATUS_MAP.put(CLOSING, "CLOSING");
+    STATUS_MAP.put(OPENING, "OPENING");
+    STATUS_MAP.put(FLOWING, "FLOWING");
+    STATUS_MAP.put(ERROR,   "ERROR");
+  }
 
   private SourceSinkFactory<Sink> sinkFactory;
 
@@ -32,6 +43,10 @@ public abstract class Plumbing implements Closeable, StatusProvider {
 
   public int getStatus() {
     return status.get();
+  }
+
+  public String getStatusString() {
+    return STATUS_MAP.get(getStatus());
   }
 
   /**

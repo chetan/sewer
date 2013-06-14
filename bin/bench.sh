2d40f69aeaf358089f1bbd56427d378a49c4284d
Chetan Sarva <chetan@pixelcop.net>
2013-06-13 22:38:17 -0400

    load hadoop native

--- bin/bench.sh
+++ bin/bench.sh
@@ -6,6 +6,12 @@ CP="-cp $ROOT/conf/:$ROOT/*:$ROOT/:$ROOT/lib/*:$ROOT/lib/"
 MAIN="net.pixelcop.sewer.Benchmark"
 mkdir -p $ROOT/var/
 
+# load hadoop native libs, if avail
+NATIVE=`find /usr/lib /usr/local/lib -name 'libhadoop.so*' | head -n 1`
+if [ "$NATIVE" != "" ]; then
+  export LD_LIBRARY_PATH=`dirname $NATIVE`
+fi
+
 RUN="java -Dlog.root=$ROOT/var $JOPTS $CP $MAIN -v"
 
 echo $RUN
